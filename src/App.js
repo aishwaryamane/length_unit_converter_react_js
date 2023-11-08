f6bca8bfbb88ab6d990f24c50f8d101dd2b7b556
aishwaryamane <aishwaryamane2498@gmail.com>
2023-11-09 03:16:43 +0530

    unit converter code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { units } from "./data.js";
+export default function App() {
+  const [firstValue, setFirstValue] = useState(0);
+  const [secondValue, setSecondValue] = useState(0);
+  const [firstUnit, setFirstUnit] = useState(1000);
+  const [secondUnit, setSecondUnit] = useState(1000);
 
-function App() {
+  //handle first inputbox value change
+  const handleFirstChange = (value) => {
+    if (value >= 0) {
+      setFirstValue(value);
+      setResult(value, firstUnit, secondUnit, setSecondValue);
+    }
+  };
+
+  //handle second inputbox value change
+  const handleSecondChange = (value) => {
+    if (value >= 0) {
+      setSecondValue(value);
+      setResult(value, secondUnit, firstUnit, setFirstValue);
+    }
+  };
+
+  //handle first unit dropdown
+  const handleFirstUnitChange = (value) => {
+    setFirstUnit(value);
+    setResult(firstValue, value, secondUnit, setSecondValue);
+  };
+
+  //handle second unit dropdown
+  const handleSecondUnitChange = (value) => {
+    setSecondUnit(value);
+    setResult(secondValue, value, firstUnit, setFirstValue);
+  };
+
+  //unit conversion and set result value
+  const setResult = (conversionValue, unit1, unit2, setResultValue) => {
+    setResultValue((conversionValue * unit1) / unit2);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h4 className="Header">Length Unit Converter</h4>
+      <div className="MainWrapper">
+        <div>
+          <input
+            className="InputBox"
+            type="number"
+            min="0"
+            value={firstValue}
+            onChange={(e) => handleFirstChange(e.target.value)}
+          />
+          <br />
+          <select
+            className="SelesctBox"
+            value={firstUnit}
+            onChange={(e) => handleFirstUnitChange(e.target.value)}
+          >
+            {units.map((unit) => {
+              return <option value={unit.value}>{unit.key}</option>;
+            })}
+          </select>
+        </div>
+        <div> = </div>
+        <div>
+          <input
+            className="InputBox"
+            type="number"
+            min="0"
+            value={secondValue}
+            onChange={(e) => handleSecondChange(e.target.value)}
+          />
+          <br />
+          <select
+            className="SelesctBox"
+            value={secondUnit}
+            onChange={(e) => {
+              handleSecondUnitChange(e.target.value);
+            }}
+          >
+            {units.map((unit) => {
+              return <option value={unit.value}>{unit.key}</option>;
+            })}
+          </select>
+        </div>
+      </div>
+    </>
   );
 }
-
-export default App;
---
+++ src/data.js
@@ -0,0 +1,6 @@
+export const units = [
+  { key: "Kilometer", value: 1000000 },
+  { key: "Meter", value: 1000 },
+  { key: "Centimeter", value: 10 },
+  { key: "Milimeter", value: 1 },
+];
